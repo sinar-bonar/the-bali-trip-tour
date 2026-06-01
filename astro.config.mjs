e925c94fe8be8b9ae0a4d3cf4f54399bf26999d2
Sinar <pdewa5437@gmail.com>
2026-06-01 13:16:12 +0800

    Switch to custom domain: baligrandtour.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://sinar-bonar.github.io',
-  base: '/bali-grand-tour',
+  site: 'https://baligrandtour.com',
+  base: '/',
   outDir: 'docs',
   trailingSlash: 'always',
   integrations: [tailwind()],
